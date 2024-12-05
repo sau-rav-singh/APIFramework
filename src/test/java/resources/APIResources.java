9fc5e8d2b4dd2eb138f1aac301bd4748cf055e66
Saurav Singh <singh.saurav@icloud.com>
2024-12-05 09:20:04 +0530

    Added Something

--- src/test/java/resources/APIResources.java
+++ src/test/java/resources/APIResources.java
@@ -1,21 +1,17 @@
 package resources;
-//enum is special class in java which has collection of constants or  methods
-public enum APIResources {
-	
-	AddPlaceAPI("/maps/api/place/add/json"),
-	getPlaceAPI("/maps/api/place/get/json"),
-	deletePlaceAPI("/maps/api/place/delete/json");
-	private String resource;
-	
-	APIResources(String resource)
-	{
-		this.resource=resource;
-	}
-	
-	public String getResource()
-	{
-		return resource;
-	}
-	
 
+import lombok.Getter;
+
+@Getter
+public enum APIResources {
+
+    AddPlaceAPI("/maps/api/place/add/json"),
+    getPlaceAPI("/maps/api/place/get/json"),
+    deletePlaceAPI("/maps/api/place/delete/json");
+
+    private final String resource;
+
+    APIResources(String resource) {
+        this.resource = resource;
+    }
 }
